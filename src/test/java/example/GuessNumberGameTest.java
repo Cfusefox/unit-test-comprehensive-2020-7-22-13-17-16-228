9f0c585fe42a13d691d5c8627cd2e0e3bf94e5de
fox <1036674617@qq.com>
2020-07-23 21:08:01 +0800

    [GuessNumberGame][York] feature: add should_return_0A0B_when_guess_number_game_given_5678_and_answer_number_is_1234 test case

--- src/test/java/example/GuessNumberGameTest.java
+++ src/test/java/example/GuessNumberGameTest.java
@@ -23,4 +23,19 @@ public class GuessNumberGameTest {
         assertEquals("4A0B", actual);
     }
 
+    @Test
+    void should_return_0A0B_when_guess_number_game_given_5678_and_answer_number_is_1234() {
+        //given
+        String guessNumber = "5678";
+        AnswerGenerate mockedAnswerGenerate = Mockito.mock(AnswerGenerate.class);
+        given(mockedAnswerGenerate.generate()).willReturn("1234");
+        GuessNumberGame guessNumberGame = new GuessNumberGame(mockedAnswerGenerate);
+
+        //when
+        String actual = guessNumberGame.checkGuessNumberAccuracy(guessNumber);
+
+        //then
+        assertEquals("0A0B", actual);
+    }
+
 }
