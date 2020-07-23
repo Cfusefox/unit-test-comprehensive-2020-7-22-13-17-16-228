1e4fe372ec03ccbae189f478a80edc74d22f5396
fox <1036674617@qq.com>
2020-07-23 23:10:26 +0800

    [GuessNumberGame][York] feature: add should_return_wrong_input_input_again_when_guess_number_game_given_2268_and_answer_number_is_1234

--- src/main/java/example/GuessNumberGame.java
+++ src/main/java/example/GuessNumberGame.java
@@ -9,18 +9,22 @@ public class GuessNumberGame {
     }
 
     public String checkGuessNumberAccuracy(String guessNumber) {
-        int allCorrectCount = 0;
-        int numberCorrectCount = 0;
-        String accuracy = "";
-        for(int i = 0; i < this.answerNumber.length(); i++) {
-            if(this.answerNumber.charAt(i) == guessNumber.charAt(i)) {
-                allCorrectCount++;
-            } else if (guessNumber.indexOf(this.answerNumber.charAt(i)) >= 0) {
-                numberCorrectCount++;
+        if(checkAnswerStringIsValid(guessNumber)) {
+            int allCorrectCount = 0;
+            int numberCorrectCount = 0;
+            String accuracy = "";
+            for(int i = 0; i < this.answerNumber.length(); i++) {
+                if(this.answerNumber.charAt(i) == guessNumber.charAt(i)) {
+                    allCorrectCount++;
+                } else if (guessNumber.indexOf(this.answerNumber.charAt(i)) >= 0) {
+                    numberCorrectCount++;
+                }
             }
+            accuracy = accuracy + allCorrectCount + "A" + numberCorrectCount + "B";
+            return accuracy;
+        } else {
+            return "Wrong Input，Input again";
         }
-        accuracy = accuracy + allCorrectCount + "A" + numberCorrectCount + "B";
-        return accuracy;
     }
 
     public Boolean checkAnswerStringIsValid(String answerString) {
