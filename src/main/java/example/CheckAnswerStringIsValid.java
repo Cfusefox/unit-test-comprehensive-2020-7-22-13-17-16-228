7257ba91ace94e03e802b54ab3a4e4f95c9f72a0
ZHANGYO5 <york.zhang@oocl.com>
2020-07-28 10:32:28 +0800

    [York] refactor: contract constant

--- src/main/java/example/CheckAnswerStringIsValid.java
+++ src/main/java/example/CheckAnswerStringIsValid.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 
 public class CheckAnswerStringIsValid implements CheckStringIsValid{
 
+    private static final char ZERO_CHARACTER = '0';
+    private static final char NINE_CHARACTER = '9';
+
     public Boolean checkAnswerStringIsValid(String answerString) {
         boolean answerStringIsRepeat = false;
         ArrayList<Character> answerCharacterList = new ArrayList<>();
         for(int index = 0; index < answerString.length(); index++) {
             char answerStringCharacter = answerString.charAt(index);
-            if(!answerCharacterList.contains(answerStringCharacter) && answerStringCharacter >= '0' && answerStringCharacter <= '9') {
+            if(!answerCharacterList.contains(answerStringCharacter) && answerStringCharacter >= ZERO_CHARACTER && answerStringCharacter <= NINE_CHARACTER) {
                 answerCharacterList.add(answerString.charAt(index));
             } else {
                 answerStringIsRepeat = true;
