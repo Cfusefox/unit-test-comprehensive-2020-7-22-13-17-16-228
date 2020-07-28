397c6202027314aae3ba6e7ef1f9806f24ebb322
ZHANGYO5 <york.zhang@oocl.com>
2020-07-28 10:30:57 +0800

    [York] refactor: rename i to index

--- src/main/java/example/CheckAnswerStringIsValid.java
+++ src/main/java/example/CheckAnswerStringIsValid.java
@@ -7,10 +7,10 @@ public class CheckAnswerStringIsValid implements CheckStringIsValid{
     public Boolean checkAnswerStringIsValid(String answerString) {
         boolean answerStringIsRepeat = false;
         ArrayList<Character> answerCharacterList = new ArrayList<>();
-        for(int i = 0; i < answerString.length(); i++) {
-            char answerStringCharacter = answerString.charAt(i);
+        for(int index = 0; index < answerString.length(); index++) {
+            char answerStringCharacter = answerString.charAt(index);
             if(!answerCharacterList.contains(answerStringCharacter) && answerStringCharacter >= '0' && answerStringCharacter <= '9') {
-                answerCharacterList.add(answerString.charAt(i));
+                answerCharacterList.add(answerString.charAt(index));
             } else {
                 answerStringIsRepeat = true;
             }
